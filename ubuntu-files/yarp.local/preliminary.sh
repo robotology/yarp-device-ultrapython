e2b2ccaa22af6b755fec5e1a32e68a5895d938bf
triccyx <triccyx@gmail.com>
2021-02-25 12:41:09 +0100

    fix

--- ubuntu-files/yarp.local/preliminary.sh
+++ ubuntu-files/yarp.local/preliminary.sh
@@ -11,22 +11,22 @@ cd /root/icubtech/python-cameras/ubuntu-files/modules
 sleep 1
 v4l2-ctl -d $VIDEODEV -c "test_pattern=0"
 sleep 1
-v4l2-ctl -d $VIDEODEV -c "trg_h=20"
+#v4l2-ctl -d $VIDEODEV -c "trg_h=20"
 sleep 1
-v4l2-ctl -d $VIDEODEV -c "trg_l=10"
+#v4l2-ctl -d $VIDEODEV -c "trg_l=10"
 sleep 1
 #v4l2-ctl -d $SUBDEV_CAM_R -c "ext_trigger=1"
 sleep 1
-v4l2-ctl -d $SUBDEV_CAM_R -c "gain=2"
+#v4l2-ctl -d $SUBDEV_CAM_R -c "gain=2"
 sleep 1
-v4l2-ctl -d $SUBDEV_CAM_R -c "analogue_gain=2"
+#v4l2-ctl -d $SUBDEV_CAM_R -c "analogue_gain=2"
 sleep 1
-v4l2-ctl -d $SUBDEV_CAM_R -c "brightness=200"
+#v4l2-ctl -d $SUBDEV_CAM_R -c "brightness=200"
 sleep 1
 #v4l2-ctl -d $SUBDEV_CAM_L -c "ext_trigger=1"
 sleep 1
-v4l2-ctl -d $SUBDEV_CAM_L -c "gain=2"
+#v4l2-ctl -d $SUBDEV_CAM_L -c "gain=2"
 sleep 1
-v4l2-ctl -d $SUBDEV_CAM_L -c "analogue_gain=2"
+#v4l2-ctl -d $SUBDEV_CAM_L -c "analogue_gain=2"
 sleep 1
-v4l2-ctl -d $SUBDEV_CAM_L -c "brightness=200"
+#v4l2-ctl -d $SUBDEV_CAM_L -c "brightness=200"
