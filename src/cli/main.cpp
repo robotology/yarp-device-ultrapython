e3e1c7fd7f76e3915fde24baa35e54433096617f
Mattia Fussi <38140169+mfussi66@users.noreply.github.com>
2021-06-14 16:07:35 +0200

    Fix return value of main and update to readme (#11)
    * merged and squashed command line interface feature
    
    * Used camelcase for vars and std endl in place of c newline
    
    * added formatting to test
    
    * updated table with yarp code value
    
    * main return value for get feature is the value of the feature itself
    
    * added space
    
    Co-authored-by: Luca Tricerri <triccyx@gmail.com>

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -89,6 +89,7 @@ int main(int argc, char* argv[]) {
     }
     std::cout << "Value for control code " << controlCode << " is: " << value
               << std::endl;
+    return value;   
   }
 
   return 0;
