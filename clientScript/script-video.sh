bbd7716ce4a185fa4e4b743659c994d571fc5f73
triccyx <triccyx@gmail.com>
2021-09-03 10:54:24 +0200

    fix

--- clientScript/script-video.sh
+++ clientScript/script-video.sh
@@ -5,28 +5,34 @@
 # BSD-3-Clause license. See the accompanying LICENSE file for details.
 
 #!/bin/bash
+screen -mS relay bash -lc '
+    killall yarpserver
+    killall yarpview
 
-killall yarpserver
-killall yarpview
+    ####YARPSERVER
+    yarp conf 10.0.1.104 10000
+    yarpserver --write &
+    sleep 1
+    #ssh root@10.0.1.233 ". ~/.bashrc;cd /root/icubtech/yarp-device-ultrapython/ini;/root/icubtech/install/bin/yarpdev --from lowultra.ini & "
+    echo    "------------------------------------------"
+    echo    "------------------------------------------"
+    echo    "PRESS ENTER when ultrapython is ready....."
+    echo    "------------------------------------------"
+    echo    "------------------------------------------"
+    read -p "."
 
-####YARPSERVER
-yarp conf 10.0.1.104 10000
-yarpserver --write &
-sleep 1
-#ssh root@10.0.1.233 ". ~/.bashrc;cd /root/icubtech/yarp-device-ultrapython/ini;/root/icubtech/install/bin/yarpdev --from lowultra.ini & "
-echo    "------------------------------------------"
-echo    "------------------------------------------"
-echo    "PRESS ENTER when ultrapython is ready....."
-echo    "------------------------------------------"
-echo    "------------------------------------------"
-read -p "."
+    ####ULTRAPYTHONUI
+    ultrapythonui --remote /grabber &
+    sleep 1
 
-####ULTRAPYTHONUI
-ultrapythonui --remote /grabber &
-sleep 1
+    ####YARPVIEW
+    yarpview &
+    sleep 1
+    yarp connect /grabber /yarpview/img:i fast_tcp
+    sleep 1
+    while true
+    do
+            sleep 1
+    done
 
-####YARPVIEW
-yarpview &
-sleep 1
-yarp connect /grabber /yarpview/img:i fast_tcp
-sleep 1
+'
